307e7af555e119462f9ae481eb4671dfa155dd8f
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-05-06 15:42:53 +0530

    made changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import History from './pages/History/History'
+// import Home from './pages/Home/Home';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/history",
+//         element: <History />
+//       },
+//       {
+//         path: "/",
+//         element: <Home />
+//       }
+//     ]
+//   }
+// ])
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import History from './pages/History/History'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import History from './pages/History/History';
 import Home from './pages/Home/Home';
 
 const router = createBrowserRouter([
@@ -12,16 +44,16 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/history",
-        element: <History />
+        index: true, // shorthand for path: '', means this will be rendered at '/'
+        element: <Home />
       },
       {
-        path: "/",
-        element: <Home />
+        path: 'history',
+        element: <History />
       }
     ]
   }
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
